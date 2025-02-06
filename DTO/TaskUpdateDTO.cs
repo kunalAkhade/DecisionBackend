0aa4abe9794ae0062ebac29b89935126860817d5
kunal <bialkunal@gmail.com>
2025-02-06 10:53:19 +0530

    fix comkar issue by using nullable reference on categoryId

--- DTO/TaskUpdateDTO.cs
+++ DTO/TaskUpdateDTO.cs
@@ -6,6 +6,6 @@
         public string Description { get; set; }
         public Models.Domain.StatusType? Status { get; set; }
         public Models.Domain.Priority? PriorityType { get; set; }
-        public string CategoryId { get; set; }
+        public string? CategoryId { get; set; }
     }
 }
